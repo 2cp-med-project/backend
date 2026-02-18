04ae455ee8231d3e30fd5b98e24807b6c04e9402
KHEDIM Mouna <m.khedim@esi-sba.dz>
2026-02-18 19:00:26 +0100

    mouna

---
+++ src/modules/appointment /appoint.controller.js
@@ -0,0 +1,65 @@
+import Service from "./appoints.service.js";
+
+// Add an appointment
+export async function addAppointment(req, res) {
+  try {
+    const patientId = req.user._id; 
+    const { type, doctorId, date } = req.body;
+
+    const appointment = await Service.createAppointment(patientId, {
+      type,
+      doctorId,
+      date,
+    });
+
+    res.status(201).json({ message: "Appointment added", appointment });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+// Modify an appointment 
+export async function updateAppointment(req, res) {
+  try {
+    const patientId = req.user._id;
+    const appointmentId = req.params.id;
+    const updates = req.body;
+
+    const appointment = await Service.updateAppointment(
+      patientId,
+      appointmentId,
+      updates
+    );
+
+    res.status(200).json({ message: "Appointment updated", appointment });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+// Delete an appointment
+export async function deleteAppointment(req, res) {
+  try {
+    const patientId = req.user._id;
+    const appointmentId = req.params.id;
+
+    await Service.deleteAppointment(patientId, appointmentId);
+
+    res.status(200).json({ message: "Appointment deleted" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+// List all appointments 
+export async function getMyAppointments(req, res) {
+  try {
+    const patientId = req.user._id;
+
+    const appointments = await appointService.getAppointments(patientId);
+
+    res.status(200).json({ appointments });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
