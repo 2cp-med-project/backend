5583321e28bbdfac23807bece198b745be8b05f0
snwng <127534631+snwng@users.noreply.github.com>
2026-02-24 17:22:01 +0100

    edit record schemas

---
+++ src/modules/records/record.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const recordSchema = new mongoose.Schema({
+  patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true },
+  consultations: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Consultation' }],
+  reports: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Report' }],
+  additionalNotes: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Record', recordSchema);
