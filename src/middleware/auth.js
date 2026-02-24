2cf539690c8a0f8948728a2944fe75b5b6a8300e
snwng <127534631+snwng@users.noreply.github.com>
2026-02-24 16:04:23 +0100

    add the auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,2 +1,22 @@
 //CHECK that the user has a valid token
 //User must be logged in to access this route
+
+const jwt = require("jsonwebtoken");
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer TOKEN
+
+  if (!token) {
+    return res.status(401).json({ message: "Access token is missing" });
+  }
+
+  try {
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch {
+    res.status(403).json({ message: "Invalid access token" });
+  }
+}
+
+module.exports = authMiddleware;
