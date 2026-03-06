4d41b1d36945efbcc0fdb554419f6a3054356353
snwng <127534631+snwng@users.noreply.github.com>
2026-03-06 16:50:35 +0100

    Add consultation CRUD, routes and Swagger docs

--- src/modules/records/records.routes.js
+++ src/modules/records/records.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import controller from './records.controller';
+import controller from './records.controller.js';
 
 router.post('/consultation', controller.createConsultation);
+router.patch('/consultation/:id', controller.updateConsultation);
+router.delete('/consultation/:id', controller.deleteConsultation);
 router.get('/consultation/:id', controller.getConsultationById);
 router.get('/consultations/:patientId', controller.getConsultations);
 
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 import express from "express";
 import usersRoutes from "./modules/users/users.routes.js";
+import recordsRoutes from "./modules/records/records.routes.js";
+
+
 
 const router = express.Router();
 
 router.use("/users", usersRoutes);
+router.use("/records", recordsRoutes);
 
 export default router;
