60c94ec968f4b3264c96bf17057cabe6d7b154dc
snwng <127534631+snwng@users.noreply.github.com>
2026-03-06 17:21:55 +0100

    Migrate auth modules to ES modules

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,1 +1,5 @@
 //Contains logic: generate token, check password
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import Doctor from "../users/doctor.model.js";
+import Patient from "../users/patient.model.js";
