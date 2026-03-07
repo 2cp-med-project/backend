7030f701ab940eea8f44170d462e57b56dda9c26
snwng <127534631+snwng@users.noreply.github.com>
2026-03-07 19:22:49 +0100

    remove auth middleware + clean code

--- src/modules/users/users.routes.js
+++ src/modules/users/users.routes.js
@@ -4,15 +4,7 @@ import * as controller from "./users.controller.js";
 const router = express.Router();
 
 // Patients
-// router.post("/patients", controller.createPatient);
 router.get("/patients", controller.getPatients);
-
-// // Doctors
-// router.post("/doctors", controller.createDoctor);
-// router.get("/doctors", controller.getDoctors);
-
-// // Admins
-// router.post("/admins", controller.createAdmin);
-// router.get("/admins", controller.getAdmins);
+router.get("/doctors", controller.getDoctors);
 
 export default router;
