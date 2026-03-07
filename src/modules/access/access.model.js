6e67bdf53db6d89cf68457a393df775ab8c7fbc3
Fad-ikram <if.benyelles@esi-sba.dz>
2026-03-07 15:56:19 +0000

    merged

---
+++ src/modules/access/access.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const accessSchema = new mongoose.Schema({
+  doctor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Doctor",
+    required: true,
+  },
+  patient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Patient",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "approved", "rejected"],
+    default: "pending",
+  },
+}, { timestamps: true });
+
+export default mongoose.model("Access", accessSchema); 
