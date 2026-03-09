723b0b0badf628a0aad4808900ef5aa271e6934b
snwng <127534631+snwng@users.noreply.github.com>
2026-03-09 01:08:43 +0100

    Add patient and doctor get-by-id endpoints

--- src/modules/users/users.routes.js
+++ src/modules/users/users.routes.js
@@ -3,8 +3,12 @@ import * as controller from "./users.controller.js";
 
 const router = express.Router();
 
-// Patients
 router.get("/patients", controller.getPatients);
+
+router.get("/patient/:id", controller.getPatientById);
+
 router.get("/doctors", controller.getDoctors);
+
+router.get("/doctor/:id", controller.getDoctorById);
 
 export default router;
