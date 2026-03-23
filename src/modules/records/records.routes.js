2f56bc0045f0538f5f29d60946c0ec0ac4cd076b
snwng <127534631+snwng@users.noreply.github.com>
2026-03-23 04:03:08 +0100

    Update records.routes.js

--- src/modules/records/records.routes.js
+++ src/modules/records/records.routes.js
@@ -1,11 +1,26 @@
 import express from "express";
-const router = express.Router();
 import controller from "./records.controller.js";
+import athenticate from "../../middleware/auth.js";
+import authorize from "../../middleware/role.js";
+import doctorAccess from "../../middleware/doctorAccess.js";
 
-router.post("/consultation", controller.createConsultation);
-router.patch("/consultation/:id", controller.updateConsultation);
-router.delete("/consultation/:id", controller.deleteConsultation);
-router.get("/consultation/:id", controller.getConsultationById);
-router.get("/record/:patientId", controller.getConsultations);
+const router = express.Router();
+
+router.use(":patientId/consultation", athenticate);
+router.use(":patientId/consultation", authorize("doctor"));
+router.use(":patientId/consultation", doctorAccess);
+router.post(":patientId/consultation", controller.createConsultation);
+
+router.use("/:patientId/consultation/:id", athenticate);
+router.use("/:patientId/consultation/:id", authorize("doctor"));
+router.use("/:patientId/consultation/:id", doctorAccess);
+router.patch("/:patientId/consultation/:id", controller.updateConsultation);
+router.get("/:patientId/consultation/:id", controller.getConsultationById);
+// router.delete("/:patientId/consultation/:id", controller.deleteConsultation);
+
+router.use("/:patientId", athenticate);
+router.use("/:patientId", authorize("doctor"));
+router.use("/:patientId", doctorAccess);
+router.get("/:patientId", controller.getConsultations);
 
 export default router;
