4a551b01ef7f2a3026975feefdc75330f7421a67
KHEDIM Mouna <m.khedim@esi-sba.dz>
2026-04-28 14:34:20 +0100

    notif and appoint comment added

--- src/modules/appointment/appoint.routes.js
+++ src/modules/appointment/appoint.routes.js
@@ -4,8 +4,8 @@ import authenticate from "../../middleware/auth.js";
 const router = express.Router();
 
 router.use(authenticate); 
-
+// Patient creates appointment
 router.post("/add", authenticate, controller.addAppointment);
-
+// Doctor views appointments
 router.get("/my",authenticate,controller.getMyAppointments);
 export default router;
--- src/modules/notifications/notif.routes.js
+++ src/modules/notifications/notif.routes.js
@@ -3,9 +3,10 @@ import controller from "./notif.controller.js";
 import service from "./notif.service.js";
 import authenticate from "../../middleware/auth.js";
 const router = express.Router();
-
+router.use(authenticate); 
+// FCM token registration
 router.post("/register-fcmtoken",authenticate, controller.registerFcmToken);
-
+// Patient response to doctor access request
 router.post("/patient-response",authenticate ,controller.patientResponse);
 
 export default router;
