26a72ac644b5c55673ac26e57fbd524bd27d52c1
Abdou MHN <mehenniabdou25@gmail.com>
2026-05-28 15:48:10 +0100

    Minor refactoring

--- src/modules/chatbot/chatbot.routes.js
+++ src/modules/chatbot/chatbot.routes.js
@@ -1,19 +1,16 @@
 import express from "express";
-import authorize from "../../middleware/role.js";
-import { authenticate } from "../../middleware/auth.js";
 import chatbotController from "./chatbot.controller.js";
+import { authorize } from "../../middleware/role.js";
+import { authenticate } from "../../middleware/auth.js";
 
 const router = express.Router();
 
 router.use(authenticate);
+router.use(authorize("patient"));
 
-router.get("/:thread_id", authorize("patient"), chatbotController.retrieveChat);
-router.post("/", authorize("patient"), chatbotController.startChat);
-router.post("/:thread_id", authorize("patient"), chatbotController.handleChat);
-router.delete(
-	"/:thread_id",
-	authorize("patient"),
-	chatbotController.deleteChat,
-);
+router.get("/:thread_id", chatbotController.retrieveChat);
+router.post("/", chatbotController.startChat);
+router.post("/:thread_id", chatbotController.handleChat);
+router.delete("/:thread_id", chatbotController.deleteChat);
 
 export default router;
